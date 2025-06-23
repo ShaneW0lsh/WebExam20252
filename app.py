2507040e5d7944f55b57f66a0339662ad7c54b5c
Daniel Kolomych <shanebetterfather@gmail.com>
2025-06-23 22:41:39 +0300

    add port variable

--- app.py
+++ app.py
@@ -338,4 +338,5 @@ if __name__ == '__main__':
         
         db.session.commit()
     
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
